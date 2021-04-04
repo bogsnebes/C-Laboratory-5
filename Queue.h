0664be7ee2b633091ecc49712ecca08dea554529
MutterGott <bogdan.bogotov123123@gmail.com>
2021-04-05 02:42:01 +0300

    Added enqueue(pss) Added enqueue(properties object)

--- Queue.h
+++ Queue.h
@@ -26,7 +26,25 @@ class Queue {
                 }
                 else {
                     list[i] = value;
-                    free[i] = true
+                    free[i] = true;
+                }
+            }
+        }
+            throw "Ячейки в памяти закончились!";
+    }
+
+    void enqueue(char *name, char *adress, int discount) {
+            for(int i = 0; i < count; i++) {
+            if (free[i] == false) {
+                if ((i + 1) != count) {
+                list[i] = list[i+1];
+                free[i+1] = false;
+                free[i] = true;
+                }
+                else {
+                    Client NewClient = Client(name, adress, discount);
+                    list[i] = NewClient;
+                    free[i] = true;
                 }
             }
         }
