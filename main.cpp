3d7b7f498166bb60e84347eb71122b855e30f354
MutterGott <bogdan.bogotov123123@gmail.com>
2021-04-07 01:23:34 +0300

    Added tests for enqueue(), copyQueue(), extend()

--- main.cpp
+++ main.cpp
@@ -7,4 +7,15 @@ using namespace std;
 
 int main() {
     Queue list = Queue(3);
+    // Test enqueue()
+    Client object = Client(_strdup("Cantik"), _strdup("Boscow"), 23);
+    list.enqueue(object);
+    list.enqueue(_strdup("Bogdan"), _strdup("Moscow"), 40);
+    // Test copyQueue()
+    (list.copyQueue()).showQueue();
+    (list.dequeue()).showClient();
+    // Test extend()
+    Queue list_2 = list.copyQueue();
+    list.extend(list_2);
+    list.showQueue();
 }
